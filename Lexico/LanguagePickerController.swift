060480f90ad288f632e9f4fa18be160d1f7675d1
Victor Guerra <vguerra@gmail.com>
2015-12-09 00:37:57 +0100

    Added first version of Translation Scene, including an Language Picker

---
+++ Lexico/LanguagePickerController.swift
@@ -0,0 +1,13 @@
+//
+//  LanguagePickerController.swift
+//  Lexico
+//
+//  Created by Victor Guerra on 09/12/15.
+//  Copyright © 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+
+class LanguagePickerController : UIViewController {
+
+}
